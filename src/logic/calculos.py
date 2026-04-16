2ea937affe04b1e8906b02026386b52ecf215ef7
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-04-16 16:34:28 -0500

    Update calculos.py

--- src/logic/calculos.py
+++ src/logic/calculos.py
@@ -1,11 +1,9 @@
 # Reglas de Negocio - TradeaYa!
 
-def calcular_comision(monto_total):
-    """Calcula el 0.5% de comisión"""
+def calcular_comision(monto_total):  """Comisión 0.5% del broker"""
     return monto_total * 0.005
 
-def validar_compra(saldo_actual, precio_accion, cantidad):
-    """Verifica si el usuario tiene dinero suficiente incluyendo comisión"""
+def validar_compra(saldo_actual, precio_accion, cantidad): """Verifica si el usuario tiene dinero suficiente incluyendo comisión"""
     costo_acciones = precio_accion * cantidad
     comision = calcular_comision(costo_acciones)
     costo_total = costo_acciones + comision
