38df26057f73ed96ac0d0c112759e26490db10a7
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-04-16 11:59:20 -0500

    Add program initialization and module imports

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+# 1. IMPORTACIÓN: Aquí es donde conectamos las carpetas
+from backend import conexion_api  # Lo que haga el Integrante 1
+from logic import reglas_negocio   # LO QUE HACES TÚ (Integrante 3)
+from frontend import interfaz      # Lo que haga el Integrante 2
 
+def iniciar_programa():
+    print("--- Bienvenido a TradeaYa! ---")
+    
+    # FLUJO LOGICO:
+    # 1. El backend trae el precio
+    precio = backend.conexion_api.obtener_precio_real("AAPL")
+    
+    # 2. Tu lógica calcula si se puede comprar
+    resultado = logic.reglas_negocio.validar_compra(precio, cantidad=10, saldo=10000)
+    
+    # 3. El frontend muestra el resultado al usuario
+    frontend.interfaz.mostrar_mensaje(resultado)
+
+if __name__ == "__main__":
+    iniciar_programa()
