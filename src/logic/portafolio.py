34ab5c5d1f6b5b53ea52a86dc6e19233f4a1ceea
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-04-29 19:54:38 -0500

    Update portafolio.py

--- src/logic/portafolio.py
+++ src/logic/portafolio.py
@@ -1,1 +1,10 @@
-
+portafolio = {
+    "AAPL": {
+        "cantidad": 10,
+        "precio_compra_promedio": 175.50  # precio promedio ponderado
+    },
+    "TSLA": {
+        "cantidad": 5,
+        "precio_compra_promedio": 220.00
+    }
+}
