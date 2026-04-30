a9e927bfa122143edebf5d61d413d5ea777cd01b
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-04-29 19:54:24 -0500

    Update historial.py

--- src/logic/historial.py
+++ src/logic/historial.py
@@ -1,1 +1,10 @@
-
+# Estructura de cada entrada
+{
+    "tipo": "COMPRA",          # o "VENTA"
+    "simbolo": "AAPL",
+    "cantidad": 5,
+    "precio": 178.20,
+    "comision": 4.45,
+    "monto_total": 895.45,
+    "timestamp": "2025-04-17T14:32:00"
+}
