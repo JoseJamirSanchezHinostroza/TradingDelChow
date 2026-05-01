e53703c86e0f6fec9167e2d8e7018cb506d4f866
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-05-01 17:32:12 -0500

    Update calculos.py

--- src/logic/calculos.py
+++ src/logic/calculos.py
@@ -1,11 +1,16 @@
 """
 Módulo de Lógica de Negocio - TradeaYa!
-Este archivo contiene los algoritmos de decisión y cálculos matemáticos.
+Se implementarán las siguientes reglas en el proyecto, de manera que puedan asegurar el correcto funcionamiento del mismo.
 .Validación de Saldo: El sistema no permitirá compras si el costo total de la transacción (precio de acción x cantidad) más la comisión excede el saldo disponible en la cuenta ficticia.
-.Gestión de Comisiones: Se aplicará una comisión fija o porcentual por cada operación realizada (ej. 0.5% por transacción) para simular los costos operativos de un broker real.
-.Horario de Mercado: Las operaciones de compra/venta solo se procesarán si el mercado de EE. UU. está abierto (Lunes a Viernes, 9:30 AM - 4:00 PM EST). Fuera de este horario, el sistema solo permitirá visualización.
+.Gestión de Comisiones: Se aplicará una comisión fija o porcentual por cada operación realizada (0.5% por transacción) para simular los costos operativos de un broker real.
+.Horario de Mercado: Las operaciones de compra/venta solo se procesarán si el mercado de EE. UU. está abierto (Lunes a Viernes, 9:30 AM - 4:00 PM EST). Fuera de este horario,
+el sistema solo permitirá visualización. El sistema, sin embargo, podría permitir almacenar órdenes de compra para ejecutarlas tan pronto como el mercado vuelva a abrirse.
 .Propiedad de Activos: El usuario solo podrá vender acciones que existan previamente en su portafolio y en cantidades iguales o menores a las que posee.
 .Actualización de Precios: Los precios de mercado deben refrescarse con una latencia máxima definida (ej. cada 15-60 segundos) para asegurar que la toma de decisiones se base en datos recientes.
+.Ejecución de Órdenes: El programa podría enseñar al usuario a ordenar la compra de acciones si es que el precio va debajo o sobre límites que se establecieron en la misma orden.
+.Gestión de riesgo: Si la Ejecución de órdenes no está activada, cuando el precio cae por debajo de un cierto límite, el sistema vendería automáticamente para proteger capital y tiraría
+una advertencia respecto a esto. En contrapunto, si sube por encima de un porcentaje, el sistema empezará a sugerir vender para obtener ganancias, y vendería automáticamente si empieza a
+caer para asegurar ganancias.
 """
 from datetime import datetime
 import pytz # Librería para manejar zonas horarias
