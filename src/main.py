39fb0eea7dd0bf476edbcec7d189a9682e71e953
Jose Sanchez Hinostroza <jjsh024680@gmail.com>
2026-05-06 18:48:04 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,27 +1,3 @@
-# 1. IMPORTACIÓN: Aquí es donde conectamos las carpetas
-from backend import conexion_api  # Trabajo del Encargado BackEnd
-from logic import sesion   # Trabajo del Encargado Logic
-from frontend import interfaz      # Trabajo del Encargado FrontEnd
-
-def iniciar_programa():
-    print("--- Bienvenido a TradeaYa! ---")
-    
-    # FLUJO LOGICO:
-    # 1. El backend trae el precio
-    precio = backend.conexion_api.obtener_precio_real("AAPL")
-    
-    # 2. Tu lógica calcula si se puede comprar
-    resultado = logic.reglas_negocio.validar_compra(precio, cantidad=10, saldo=10000)
-    
-    # 3. El frontend muestra el resultado al usuario
-    frontend.interfaz.mostrar_mensaje(resultado)
-
-if __name__ == "__main__":
-    iniciar_programa()
-
-
-
-
 """
 main.py - Prueba de la lógica de TradeaYa! sin backend real
 Simula precios y timestamps para verificar que todo funciona.
